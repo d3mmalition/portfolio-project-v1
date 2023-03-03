4904f66483d6733284af0027ec8a03ba3b3c693b
d3mmalition <ecarr.gardner@gmail.com>
2023-03-03 00:28:58 -0500

    font udpates

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
 import React from 'react';
-
 // Custom Theme
 import defaultTheme from '../src/theme/theme';
 import {ThemeProvider} from '@mui/material/styles';
-
 // MUI Components
 import useMediaQuery from '@mui/material/useMediaQuery';
 import {Container, CssBaseline} from '@mui/material';
-
 // Components
 import Header from './components/Header';
-import Hero from './components/Hero';
+import Intro from './components/Intro';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
-
 // CSS
 import './App.css';
 
 export default function App() {
   const isIpadScreenLess = useMediaQuery(defaultTheme.breakpoints.down('md'));
-
   const headerFooterConfigs = {
-    sx: {marginY: 4},
+    sx: { marginY: 4 },
   };
-
   const sectionConfigs = {
     component: 'section',
-    sx: {marginY: 12, display: 'inline-block'},
+    sx: { marginY: 12, display: 'inline-block' },
   };
-
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <Container fixed sx={{fontFamily: 'MinigapLight'}}>
-        <Header configs={headerFooterConfigs} />
-        <main>
-          <Hero
-            configs={sectionConfigs}
-            id='hero-section'
-            isIpadScreenLess={isIpadScreenLess}
-          />
-          <Projects configs={sectionConfigs} id='projects-section' />
-        </main>
-        <Footer configs={headerFooterConfigs} />
-      </Container>
-    </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Container fixed sx={{ fontFamily: 'Poppins' }}>
+          <Header configs={headerFooterConfigs} />
+          <main>
+            <Intro
+              configs={sectionConfigs}
+              id="intro-section"
+              isIpadScreenLess={isIpadScreenLess}
+            />
+            <Projects configs={sectionConfigs} id="projects-section" />
+          </main>
+          <Footer configs={headerFooterConfigs} />
+        </Container>
+      </ThemeProvider>
   );
 }
